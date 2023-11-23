9c4d8a9905dd3683a5bab36bf265afe44459f873
Frontend Development With React Batch 2309 <150684880+fdr2309@users.noreply.github.com>
2023-11-23 19:44:28 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,26 +4,7 @@ const bigScreen = document.querySelector(".bigScreen");
 const showImg = document.querySelector(".showImg");
 const close = document.querySelector(".close");
 
-// fetch(`https://api.slingacademy.com/v1/sample-data/photos?limit=12`)
-//   .then((res) => res.json())
-//   .then((data) => {
-//     let allPhotos = data.photos;
-//     // console.log(allPhotos);
 
-//     allPhotos.map((img) => {
-//       let layout = document.createElement("div");
-//       layout.classList.add("layout");
-
-//       layout.innerHTML = `<img class = "img"
-//         src="${img.url}"
-//         alt=""
-//         />`;
-
-//       container.appendChild(layout);
-//     });
-//   });
-
-console.log(allImeges);
 
 allImeges.forEach((item) => {
   //   console.log(item);
